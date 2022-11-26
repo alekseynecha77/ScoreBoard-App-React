81cee84abd18207bcb1d7724707108383472705d
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-11-25 20:45:03 -0500

    Update Stopwatch.js

--- my-scoreboard/src/component/Stopwatch.js
+++ my-scoreboard/src/component/Stopwatch.js
@@ -1,12 +1,25 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 const Stopwatch = ()=>{
+    const [isRunning, setIsRunning] = useState(false);
+    const [elapsedTime, setElapsedTime] = useState(0);
+
+    useEffect(()=>{
+        if(isRunning){
+            setInterval(() => setElapsedTime(prevTime => prevTime + 1), 1000);
+        }
+    },[isRunning]);
+
     return(
         <div className="stopwatch">
             <h2>Stopwatch</h2>
-            <span className="stopwatch-time">0</span>
-            <button>Start</button>
-            <button>Reset</button>
+            <span className="stopwatch-time">{elapsedTime}</span>
+            <button onClick={() => setIsRunning((prevValue) => !prevValue)}>
+                {isRunning ? "Stop": "Start"}
+            </button>
+            <button onClick={() => setElapsedTime(0)}>
+                Reset
+            </button>
         </div>
     );
 }
