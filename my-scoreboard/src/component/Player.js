d492268aa7b6e9f721fc947da8ce0328c8e8c7f7
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-11-26 19:48:42 -0500

    final

--- my-scoreboard/src/component/Player.js
+++ my-scoreboard/src/component/Player.js
@@ -1,12 +1,17 @@
 import React, {memo} from "react";
 import Counter from "./Counter";
+import Icon from "./Icon";
+import PropTypes  from "prop-types";
 
-const Player = ({name, score, id, removePlayer, changeScore}) => {
+const Player = ({name, score, id, removePlayer, changeScore, isHighestScore}) => {
     return (
       <div className="player">
       {console.log(name + 'render')}
         <span className="player-name">
           <button className="remove-player" onClick={() => removePlayer(id)}>✖</button>
+          <Icon isHighestScore={isHighestScore} />
+
+  
           {name}
         </span>
   
@@ -19,7 +24,15 @@ const Player = ({name, score, id, removePlayer, changeScore}) => {
       </div>
     );
   }
+  Player.propTypes = {
+    name: PropTypes.string.isRequired,
+    score: PropTypes.number.isRequired,
+    id: PropTypes.number.isRequired,
+    removePlayer: PropTypes.func.isRequired,
+    changeScore: PropTypes.func.isRequired,
+    isHighestScore: PropTypes.bool.isRequired
+  }
 const playerPropsAreEqual = (prevProps, nextProps) =>{
-  return prevProps.score === nextProps.score;
+  return prevProps.score === nextProps.score && prevProps.isHighestScore === nextProps.isHighestScore;
 }
   export default memo(Player, playerPropsAreEqual);
